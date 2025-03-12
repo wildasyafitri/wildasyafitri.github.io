5aefe51b7359808fd0c2e44ebb57bc44126b7937
wildasyafitri <wildasyafitri@students.usu.ac.id>
2025-03-12 14:08:07 +0700

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html>
+  <head>
+  <script>
+  document.write ("script diantara tag head")
+  </scrpt>
+  </head>
+<body>
+  <script>
+  document.write ("script diantara body"
+                  </script>
+                  </body>
+                  </html>
